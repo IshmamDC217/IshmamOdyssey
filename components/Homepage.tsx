a3eae35d316daebd83eb62965f0102881409f9ec
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-05-17 04:22:03 +0100

    Homepage setup

---
+++ components/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <section className='homepage'>
+      <div>
+        <p className=''>
+          <strong>Hello</strong> I am <strong>Ishmam</strong>
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Homepage
