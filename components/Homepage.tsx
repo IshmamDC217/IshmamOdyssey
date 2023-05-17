32644a5bde8b471c46de0c6f164340fa057325ee
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-05-17 07:20:44 +0100

    Netlify fix 2

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -1,43 +1,43 @@
 import React from 'react';
 import Image from 'next/image';
-import Typewriter, { Options } from 'typewriter-effect';
+import Typewriter from 'typewriter-effect';
 import mainMe from '../public/mainMe.png';
 
 const Homepage = () => {
-  const typewriterOptions: Partial<Options> = {
-    strings: ['Software Engineer', 'Frontend Developer', 'Ui/Ux Designer', 'Based in Leeds, UK'],
-    changeDelay: 3 as number,
-    changeDeleteSpeed: 2,
-    autoStart: true,
-    loop: true,
-  };
-
   return (
     <section className="homepage">
       <div className="relative h-screen grid grid-cols-2">
         <div className="flex items-center justify-center">
           <div className="mx-auto">
-            <div className="desc text-white flex flex-col">
-              <div>
+            <div className="text-white flex flex-col">
+              <div className='desc'>
                 <h2 className="text-4xl mb-5">Hi there, I'm </h2>
-                <h1 className="text-5xl mb-10 text-orange-300" style={{ letterSpacing: '0.05em' }}>
+                <h1 className="text-5xl mb-5 text-orange-300" style={{ letterSpacing: '0.05em' }}>
                   Ishmam Ahmed
                 </h1>
               </div>
-              <div className="bg-[#cf6a17] h-[4px] w-[250px] mb-2"></div>
+              <div className="bg-[#cf6a17] h-[4px] w-[250px] mb-6"></div>
               <h4 className="typewriters text-2xl mb-5 text-gray-400">
-                <Typewriter options={typewriterOptions} />
+                {/* <Typewriter
+                  options={{
+                    strings: ['Software Engineer', 'Frontend Developer', 'Ui/Ux Designer', 'Based in Leeds, UK'],
+                    changeDelay: 3,
+                    changeDeleteSpeed: 2,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                /> */}
               </h4>
-              <p className='maindesc max-w-[500px] mb-44 py-5=3'>
-                - I'm a <strong>Software Engineer</strong>, pursuing a postgraduate degree in <strong>AI</strong> from the <strong>University of Leeds</strong>. My interests include Web Development, Mobile Applications, Game Development, and AI. Combining my passion for technology and music as a session musician, I stay updated with the latest trends, continuously learning and expanding my skillset.
-              </p>
+              <p className='maindesc max-w-[500px] mb-40 py-5'>
+                  - As a dedicated <strong className='text-lg'>Software Engineer</strong>, I am currently pursuing a postgraduate degree in <strong className='text-lg'>Artificial Intelligence</strong> from the <strong className='text-lg'>University of Leeds</strong> with a strong focus on <strong className='text-lg'>Web Development</strong>, <strong className='text-lg'>Mobile Applications</strong>, and <strong className='text-lg'>AI</strong>,
+                </p>
             </div>
           </div>
         </div>
         <div className="flex items-center justify-center mb-60">
           <div className="mx-auto">
             <a>
-              <Image src={mainMe} className="w-2/3 h-2/3" />
+              <Image src={mainMe} className="w-full h-full" />
             </a>
           </div>
         </div>
