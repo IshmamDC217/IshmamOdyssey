efd94a1e5593aee6f1af15bb280b037771b0dba9
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-05-26 18:32:18 +0100

    3 Projects added

--- components/Work.tsx
+++ components/Work.tsx
@@ -3,6 +3,14 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 import metoraweb1 from '../public/metoraweb1.png';
 import metoraweb2 from '../public/metoraweb2.png';
+import crypticchat1 from '../public/crypticchat1.png';
+import crypticchat2 from '../public/crypticchat2.png';
+import golweb1 from '../public/golweb1.png';
+import golweb2 from '../public/golweb2.png';
+import metoralogo from '../public/metoralogo.png';
+import lock192 from '../public/lock192.png';
+import gollogo from '../public/gollogo.jpg';
+import Link from 'next/link';
 import 'atropos/css';
 import Atropos from 'atropos/react';
 
@@ -20,12 +28,41 @@ const Work: FunctionComponent = () => {
     },
   ];
 
+  const crypticchat = [
+    {
+      image: crypticchat1,
+      title: 'Cryptic Chat: Log In',
+      link: 'https://cryptichat.benmitchell.dev/',
+    },
+    {
+      image: crypticchat2,
+      title: 'Metora Cliffs Web: About',
+      link: 'https://cryptichat.benmitchell.dev/',
+    },
+  ];
+
+  const goljs = [
+    {
+      image: golweb1,
+      title: 'Metora Cliffs Web: Homepage',
+      link: 'https://metoracliffs.netlify.app',
+    },
+    {
+      image: golweb2,
+      title: 'Metora Cliffs Web: About',
+      link: 'https://metoracliffs.netlify.app',
+    },
+  ];
+
   return (
     <section className="work">
-      <div className='glass-container' style={{ padding: '60px' }}>
+      <div className='glass-container' style={{ padding: '20px' }}>
         <div className="container">
-          <h2 className="text-3xl font-bold mb-14 text-black">My Projects</h2>
-          <div className="project-container grid grid-cols-2 gap-32">
+          <div style={{ display: 'flex', alignContent: 'center' }}>
+            <img src={metoralogo.src} className='w-[80px] mb-5 ' />
+            <h2 className="text-3xl font-bold mx-6 my-10">Official Band Website</h2>
+          </div>
+          <div className="container rounded-lg project-container grid grid-cols-2 gap-32" style={{ backgroundColor: '#12121262', padding: '80px' }}>
             {metoraweb.map((work, index) => (
               <motion.a
                 key={index}
@@ -57,9 +94,15 @@ const Work: FunctionComponent = () => {
             ))}
           </div>
         </div>
-        <div className="project-description my-16 max-w-6xl">
-          <h3 className="text-xl font-semibold mb-2 text-align text-black">Metora Cliffs Web</h3>
-          <p className="text-black text-align text-lg">
+        <div className="project-description my-16 max-w-7xl">
+          <Link href='https://metoracliffs.netlify.app'
+            target="_blank"
+            rel="noopener noreferrer" className="text-white text-2xl font-semibold mb-2 rounded
+             hover:bg-gray-100 md:hover:bg-transparent md:border-0
+             md:hover:text-black md:p-0 dark:text-white md:dark:hover:text-black
+             dark:hover:bg-gray-700 dark:hover:text-white 
+             md:dark:hover:bg-transparent flex justify-center items-center">Metora Cliffs Web</Link>
+          <p className="innerglasscontainer text-black text-center text-lg">
             Metora Cliffs Web is the official website for Metora Cliffs, a video game-inspired progressive metal band
             that composes songs with a Nintendo-esque vibe. The website is developed with a video game theme to reflect
             the band's unique style and energy. It is built using modern web technologies such as Next.js, React, and
@@ -67,6 +110,131 @@ const Work: FunctionComponent = () => {
             Particle.js, React Spring, Atropos JS, and React Toastify for a visually stunning and engaging user experience.
             The website is optimized for both desktop and mobile devices, ensuring that users have a consistent experience
             across all platforms.
+          </p>
+        </div>
+      </div>
+      <div className='glass-container2 rounded-lg my-4' style={{ padding: '20px' }}>
+        <div className="container">
+          <div style={{ display: 'flex', alignContent: 'center' }}>
+            <img src={lock192.src} className='w-[100px] mb-4 border rounded-full' />
+            <h2 className="text-3xl font-bold mx-6 my-7">End-to-End Encrypted Messaging Platform</h2>
+          </div>
+          <div className="container rounded-lg project-container grid grid-cols-2 gap-32" style={{ backgroundColor: '#12121262', padding: '80px' }}>
+            {crypticchat.map((work, index) => (
+              <motion.a
+                key={index}
+                href={work.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-link w-inline-block"
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1.2 }}
+                transition={{ duration: 0.8, delay: index * 0.2, type: 'spring', stiffness: 150 }}
+              >
+                <div className="project-image">
+                  <Atropos
+                    activeOffset={40}
+                    shadowScale={1.05}
+                    onEnter={() => console.log('Enter')}
+                    onLeave={() => console.log('Leave')}
+                    onRotate={(x, y) => console.log('Rotate', x, y)}
+                  >
+                    <motion.div
+                      className="image-overlay"
+                      whileHover={{ scale: 1.05 }}
+                      whileTap={{ scale: 0.95 }}
+                    ></motion.div>
+                    <Image src={work.image} alt={work.title} />
+                  </Atropos>
+                </div>
+              </motion.a>
+            ))}
+          </div>
+        </div>
+        <div className="project-description my-16 max-w-7xl">
+          <Link href='https://cryptichat.benmitchell.dev/'
+            target="_blank"
+            rel="noopener noreferrer" className="text-white text-2xl font-semibold mb-2 rounded
+             hover:bg-gray-100 md:hover:bg-transparent md:border-0
+             md:hover:text-black md:p-0 dark:text-white md:dark:hover:text-black
+             dark:hover:bg-gray-700 dark:hover:text-white 
+             md:dark:hover:bg-transparent flex justify-center items-center">Cryptic Chat</Link>
+          <p className="innerglasscontainer text-black text-center text-lg">
+            Cryptic Chat is a highly secure and privacy-focused messaging platform developed using React JS.
+            By implementing robust encryption algorithms, it ensures end-to-end encryption,
+            guaranteeing that user communications remain confidential and safeguarded against
+            unauthorized access. The integration of an open-source GraphQL API enables
+            seamless and efficient data transmission, allowing Cryptic Chat to seamlessly
+            integrate with diverse systems. Additionally, the platform leverages a
+            blockchain-based key store, bolstering the security framework by securely
+            managing encryption keys and providing an added layer of protection against
+            unauthorized entities. With a strong emphasis on user privacy and utilizing
+            cutting-edge technologies, Cryptic Chat offers a professional and trustworthy
+            messaging solution that prioritizes data security in today's digital landscape.
+          </p>
+        </div>
+      </div>
+      <div className='glass-container2 rounded-lg' style={{ padding: '20px' }}>
+        <div className="container">
+          <div style={{ display: 'flex', alignContent: 'center' }}>
+            <img src={gollogo.src} className='w-[100px] mb-4' />
+            <h2 className="text-3xl font-bold mx-6 my-7">Conway's Game of Life - Javascript</h2>
+          </div>
+          <div className="container rounded-lg project-container grid grid-cols-2 gap-32" style={{ backgroundColor: '#12121262', padding: '80px' }}>
+            {goljs.map((work, index) => (
+              <motion.a
+                key={index}
+                href={work.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-link w-inline-block"
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1.2 }}
+                transition={{ duration: 0.8, delay: index * 0.2, type: 'spring', stiffness: 150 }}
+              >
+                <div className="project-image">
+                  <Atropos
+                    activeOffset={40}
+                    shadowScale={1.05}
+                    onEnter={() => console.log('Enter')}
+                    onLeave={() => console.log('Leave')}
+                    onRotate={(x, y) => console.log('Rotate', x, y)}
+                  >
+                    <motion.div
+                      className="image-overlay"
+                      whileHover={{ scale: 1.05 }}
+                      whileTap={{ scale: 0.95 }}
+                    ></motion.div>
+                    <Image src={work.image} alt={work.title} />
+                  </Atropos>
+                </div>
+              </motion.a>
+            ))}
+          </div>
+        </div>
+        <div className="project-description my-16 max-w-7xl">
+          <Link href='https://comp5400-game-of-life.netlify.app/'
+            target="_blank"
+            rel="noopener noreferrer" className="text-white text-2xl font-semibold mb-2 rounded
+             hover:bg-gray-100 md:hover:bg-transparent md:border-0
+             md:hover:text-black md:p-0 dark:text-white md:dark:hover:text-black
+             dark:hover:bg-gray-700 dark:hover:text-white 
+             md:dark:hover:bg-transparent flex justify-center items-center">Game Of Life JS</Link>
+          <p className="innerglasscontainer text-black text-center text-lg">
+            This implementation showcases Conway's Game of Life using JavaScript, 
+            an intriguing cellular automaton conceived by the esteemed British mathematician 
+            John Horton Conway in 1970. The game unfolds on a two-dimensional grid consisting 
+            of cells, each existing in either an alive or dead state. The game progresses 
+            through successive generations based on a set of rules that govern the survival 
+            or demise of each cell. The underlying principles of the game involve the following 
+            rules: Firstly, any live cell with fewer than two live neighbors perishes due to 
+            underpopulation. Secondly, a live cell with two or three live neighbors persists 
+            into the subsequent generation. Thirdly, any live cell with more than three live 
+            neighbors succumbs to overpopulation and dies. Lastly, a dead cell precisely 
+            surrounded by three live neighbors springs to life through reproduction. 
+            This implementation delves into the intricacies of Conway's Game of Life, 
+            providing a professional and insightful exploration of this captivating 
+            mathematical simulation.
           </p>
         </div>
       </div>
