3ab5997ec2fa6c9bd344f346d00e76623c1d7019
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-05-27 14:58:35 +0100

    Navigation bar connects to contact page

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -104,7 +104,7 @@ const Navigation = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="/contact"
                 className="block text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Contact
---
+++ pages/contact.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Navigation from "../components/Navigation"
+import Contact from '../components/Contact'
+
+
+export default function ContactPage() {
+
+    return (
+        <div>
+            <Head>
+                <meta name="description" content="Contact page" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <header>
+                <Navigation />
+            </header>
+
+            <main>
+                <Contact />
+                {/* <Footer /> */}
+            </main>
+        </div>
+    )
+}
