df804296991792264f2a013ecaa37b50bac08ced
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-06-03 20:38:03 +0100

    Contact fixes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,59 +1,23 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState, FormEvent } from 'react';
 import { motion } from 'framer-motion';
-import emailjs from 'emailjs-com';
 import { SiGmail, SiLinkedin } from 'react-icons/si';
 import { AiFillGithub } from 'react-icons/ai';
-
-interface FormData {
-    name: string;
-    email: string;
-    message: string;
-}
+import emailjs from 'emailjs-com';
 
 const ContactMe: React.FC = () => {
-    const [formData, setFormData] = useState<FormData>({
-        name: '',
-        email: '',
-        message: '',
-    });
     const [isSubmitted, setIsSubmitted] = useState(false);
 
-    const handleChange = (
-        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
-
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    const sendEmail = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        emailjs
-            .sendForm(
-                'service_o9qwf07',
-                'template_8x1x04a',
-                e.currentTarget,
-                'Ishmam Ahmed'
-            )
+        emailjs.sendForm('service_ycg97wl', 'service_ycg97wl', e.currentTarget, 'C8eJ23jZHxa1XTy_s')
             .then((result) => {
-                // Handle success
                 console.log(result.text);
                 setIsSubmitted(true);
             })
             .catch((error) => {
-                // Handle error
-                console.error(error.text);
+                console.log(error.text);
             });
-
-        // Clear form data
-        setFormData({
-            name: '',
-            email: '',
-            message: '',
-        });
     };
 
     return (
@@ -74,76 +38,53 @@ const ContactMe: React.FC = () => {
                         <SiLinkedin className="text-3xl" style={{ color: '#ffffff' }} />
                     </a>
                 </motion.div>
-                <div className="max-w-lg mx-auto">
-                    {isSubmitted ? (
-                        <p className="text-green-500 text-lg font-semibold mb-4">
-                            Thank you for your message! I'll get back to you soon.
-                        </p>
-                    ) : (
-                        <form onSubmit={handleSubmit}>
-                            <input type="hidden" name="form_name" value="contact" />
-                            <div className="mb-4">
-                                <label
-                                    htmlFor="name"
-                                    className="block text-lg font-semibold mb-2 text-white"
-                                >
-                                    <i className="fas fa-user"></i> Name
-                                </label>
-                                <input
-                                    type="text"
-                                    id="name"
-                                    name="name"
-                                    value={formData.name}
-                                    onChange={handleChange}
-                                    required
-                                    className="w-full px-4 py-2 border text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label
-                                    htmlFor="email"
-                                    className="block text-lg font-semibold mb-2 text-white"
-                                >
-                                    Email
-                                </label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    name="email"
-                                    value={formData.email}
-                                    onChange={handleChange}
-                                    required
-                                    className="w-full px-4 py-2 border text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label
-                                    htmlFor="message"
-                                    className="block text-lg font-semibold mb-2 text-white"
-                                >
-                                    Message
-                                </label>
-                                <textarea
-                                    id="message"
-                                    name="message"
-                                    value={formData.message}
-                                    onChange={handleChange}
-                                    required
-                                    rows={4}
-                                    className="w-full px-4 py-2 border text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-                                ></textarea>
-                            </div>
-                            <div className="text-center">
-                                <button
-                                    type="submit"
-                                    className="px-6 py-2 text-lg font-semibold text-white bg-orange-500 rounded-lg hover:bg-orange-600"
-                                >
-                                    Submit
-                                </button>
-                            </div>
-                        </form>
-                    )}
-                </div>
+                {isSubmitted ? (
+                    <p className="text-green-500 text-lg font-semibold mb-4">
+                        Thank you for your message! I'll get back to you soon.
+                    </p>
+                ) : (
+                    <form onSubmit={sendEmail}>
+                        <div className="flex flex-col mb-4">
+                            <label htmlFor="name" className="text-white">
+                                Name
+                            </label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                className="border rounded-md py-2 px-3"
+                                required
+                            />
+                        </div>
+                        <div className="flex flex-col mb-4">
+                            <label htmlFor="email" className="text-white">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                className="border rounded-md py-2 px-3"
+                                required
+                            />
+                        </div>
+                        <div className="flex flex-col mb-4">
+                            <label htmlFor="message" className="text-white">
+                                Message
+                            </label>
+                            <textarea
+                                id="message"
+                                name="message"
+                                rows={4}
+                                className="border rounded-md py-2 px-3"
+                                required
+                            ></textarea>
+                        </div>
+                        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">
+                            Submit
+                        </button>
+                    </form>
+                )}
             </div>
         </section>
     );
