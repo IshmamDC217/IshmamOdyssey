bc7e374b7c329e18845ba309e5e4d235f54d282e
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-07-11 01:30:56 +0100

    Font test

--- components/HomepageMob.tsx
+++ components/HomepageMob.tsx
@@ -25,7 +25,7 @@ const HomepageMob = () => {
                   initial={{ opacity: 0, y: 50 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.8, delay: 0.3 }}
-                  className='pfbio max-w-[500px] mb-12 mx-auto p-4 py-2 text-white text-xs'
+                  className='pfbio max-w-[500px] mb-12 mx-auto p-4 py-2 text-white text-sm'
                 >
                   Allow me to introduce myself as a <b>software engineer</b> and <b>musician</b>. With a <b>BSc in Computer Science</b> and an <b>MEng in Artificial Intelligence</b>, I have a strong foundation in both technical problem-solving and innovative thinking. As a guitarist, I channel my creative passion through music. I work as a software engineer at <a href='https://www.hlrlookup.com' rel="noopener noreferrer" className='font-bold text-orange-500'>HLR Lookup</a> in Leeds and play guitar for the progressive rock band, <a href='https://metoracliffs.netlify.app' rel="noopener noreferrer" className='font-bold text-orange-500'>Metora Cliffs</a>. Blending my technical expertise and musical passion, I create captivating experiences at the intersection of technology and music.
                 </motion.p>
