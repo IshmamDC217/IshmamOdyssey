f6d72f2620e8703196996e9de5dad1ebfb4c662d
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-07-15 17:37:29 +0100

    Heading component

---
+++ components/Heading.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+interface HeadingProps {
+  title: string;
+}
+
+const Heading: FC<HeadingProps> = ({ title }) => {
+  return (
+    <div className='text-white text-5xl mb-10 font-medium flex items-center'>{title}
+    </div>
+  );
+}
+
+export default Heading;
