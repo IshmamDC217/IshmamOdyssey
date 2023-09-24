c25dcd8ac4bb9a7c08014468cb57ef669bd54a80
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-09-24 01:06:18 +0100

    Makeover pt.3

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,9 +14,9 @@ const Sidebar: React.FC<SidebarProps> = ({ sidebarOpen, toggleSidebar }) => {
             className={`fixed inset-0 transform transition-transform duration-200 ease-in-out z-50 md:relative md:translate-x-0 ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'
                 }`}
         >
-            <div className="sidebarmob h-full bg-white dark:bg-slate-900 dark:border-gray-700 w-64 p-4 overflow-auto">
+            <div className="sidebarmob h-full bg-[#c9834a] dark:bg-slate-[#f09a53a3] dark:border-gray-700 w-64 p-4 overflow-auto">
                 <button
-                    className="inline-flex items-center p-2 text-sm text-gray-400 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    className="inline-flex items-center p-2 text-sm border text-white-400 rounded-lg hover:bg-white-100 focus:outline-none focus:ring-2 focus:ring-white-200 dark:text-white-400 dark:hover:bg-white-700 dark:focus:ring-white-600"
                     onClick={toggleSidebar}
                 >
                     <FaTimes className="w-6 h-6" />
@@ -25,19 +25,19 @@ const Sidebar: React.FC<SidebarProps> = ({ sidebarOpen, toggleSidebar }) => {
                     {/* Add your mobile menu options here */}
                     <li>
                         <a href="/" className="flex items-center">
-                            <BsHouseDoorFill className="w-5 h-5 mr-2 text-blue-700 dark:text-blue-500" />
+                            <BsHouseDoorFill className="w-5 h-5 mr-2 text-white dark:text-white" />
                             Home
                         </a>
                     </li>
                     <li>
                         <a href="/work" className="flex items-center">
-                            <RiComputerFill className="w-5 h-5 mr-2 text-blue-700 dark:text-blue-500" />
+                            <RiComputerFill className="w-5 h-5 mr-2 text-white dark:text-white" />
                             Work
                         </a>
                     </li>
                     <li>
                         <a href="/contact" className="flex items-center">
-                            <RiContactsBook2Fill className="w-5 h-5 mr-2 text-blue-700 dark:text-blue-500" />
+                            <RiContactsBook2Fill className="w-5 h-5 mr-2 text-white dark:text-white" />
                             Contact
                         </a>
                     </li>
